bd1467aacca92f8dbdb28d7cd3ebb0964678ba8f
Marc Ferland <mferland1980@gmail.com>
2019-12-29 11:48:16 -0500

    added annotations

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"})
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '. $number .'</body></html>'
+        );
+    }
+}

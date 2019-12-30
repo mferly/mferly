750565d00108ca00211508fa3439b1b187664c24
Marc Ferland <mferland1980@gmail.com>
2019-12-30 12:43:03 -0500

    Few tweaks and now we push to docker.io repo

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-docker build --pull --force-rm --no-cache -f ./Dockerfile-nginx -t xx-array_nginx .
-docker build --pull --force-rm --no-cache -f ./Dockerfile-php -t xx-array_php .
+docker build --pull --force-rm --no-cache -f ./Dockerfile-nginx -t mferly/xx-array_nginx .
+docker build --pull --force-rm --no-cache -f ./Dockerfile-php -t mferly/xx-array_php .
 
-# docker push mferly/xx-nginx:2
-# docker push mferly/xx-php:2
-# docker push mferly/xx-app:2
+docker push mferly/xx-array_nginx:latest && \
+docker push mferly/xx-array_php:latest
 
 # docker-compose build --no-cache --pull --force-rm --build-arg CACHE_DATE=$(date +%Y-%m-%d:%H:%M:%S)

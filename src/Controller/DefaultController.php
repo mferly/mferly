42b9437c9d3eb9322b872bfc7b4cd2445a4ba9e8
Marc Ferland <mferland1980@gmail.com>
2019-12-29 21:00:54 -0500

    Updated favicon

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -20,7 +20,8 @@ class DefaultController extends AbstractController
                 <!doctype html>
                 <html>
                     <head>
-                        <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
+                        <title>xx-array</title>
+                        <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA/ElEQVQ4Ed3SsSuFYRQG8F/dGK4YbJLJSCnlDyCDhdFkN8km012VLIrc9S7KZvWP3K4km0FiEINu6Hydr957DcrGqdP3nvM85+l7zvvy72IKuzjD6VBGL7DgfItxHKKduZyMdWxgBEs4SdHgxkwVkzhHC894x1jmJyKnMYpXPGIvZ2LWMbbxkeQQCGAx6xBYwQTestfHVs5Wfq8Kcgx0cVf07tEr6uBc5K4qgcshMAg/ZacWCAureMJ12rnNvwg7Ye0GD9jP/gvWagv1EqNxlIudzaXt5O00sJDYATbLJUa/vsYA55MYnyZminoOwRm4xgKvHsmvHlIp8sfOXxaST6xSO0JHAAAAAElFTkSuQmCC" rel="icon" type="image/x-icon" />
                         <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
                         <style>
                             body {

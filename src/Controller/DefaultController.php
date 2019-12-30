f1c0fa01372200c253400363ed4b8b8649c3b216
Marc Ferland <mferland1980@gmail.com>
2019-12-29 20:53:07 -0500

    added styling

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,7 +16,23 @@ class DefaultController extends AbstractController
         $number = random_int(0, 100);
 
         return new Response(
-            '<html><body>Marc Lucky number: '. $number .'</body></html>'
+            '
+                <!doctype html>
+                <html>
+                    <head>
+                        <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
+                        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+                        <style>
+                            body {
+                                font-family: \'Open Sans\', sans-serif;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        Marc\'s (random) lucky number is <b>'. $number .'</b>
+                    </body>
+                </html>
+            '
         );
     }
 }
